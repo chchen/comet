195ec4ecbe119731254418296744cc3173628d90
cchen <cchen@nougat.org>
2020-03-26 18:13:51 -0700

    Adds natural number and boolean bit buffers to unity model

--- unity-synthesis/unity/semantics.rkt
+++ unity-synthesis/unity/semantics.rkt
@@ -5,6 +5,27 @@
          "syntax.rkt"
          rosette/lib/match)
 
+(define (natural? n)
+  (exact-nonnegative-integer? n))
+
+(define (eval-not v)
+  (not v))
+
+(define (eval-and l r)
+  (and l r))
+
+(define (eval-or l r)
+  (or l r))
+
+(define (eval-+ l r)
+  (+ l r))
+
+(define (eval-< l r)
+  (< l r))
+
+(define (eval-= l r)
+  (= l r))
+
 (define (channel-empty? val)
   (and (channel*? val)
        (not (channel*-valid val))))
@@ -19,33 +40,88 @@
 (define (eval-value val)
   (channel*-value val))
 
-(define (eval-not v)
-  (not v))
+(define (eval-send-empty? buf)
+  (eq? 0 (length (send-buf*-val buf))))
 
-(define (eval-and l r)
-  (and l r))
+(define (eval-recv-full? buf)
+  (eq? (recv-buf*-len buf)
+       (length (recv-buf*-val buf))))
 
-(define (eval-or l r)
-  (or l r))
+(define (eval-nat->send-buf len val)
+  (define (nat->bool-list v pow)
+    (if (< pow 0) ;; base case
+        '()
+        (let ([e (expt 2 pow)])
+          (if (>= v e)
+              (cons #t (nat->bool-list (- v e)
+                                       (- pow 1)))
+              (cons #f (nat->bool-list v
+                                       (- pow 1)))))))
 
-(define (eval-eq? l r)
-  (eq? l r))
+  (let ([maxval (- (expt 2 len) 1)])
+    (if (> val maxval)
+        'send-buf-overflow
+        (send-buf* len (nat->bool-list val (- len 1))))))
+
+(define (eval-send-buf-head buf)
+  (match buf
+    [(send-buf* len val)
+     (if (null? val)
+         'send-buf-no-head
+         (car val))]))
+
+(define (eval-send-buf-tail buf)
+  (match buf
+    [(send-buf* len val)
+     (if (null? val)
+         'send-buf-no-tail
+         (send-buf* len (cdr val)))]))
+
+(define (eval-empty-recv-buf len)
+  (recv-buf* len '()))
+
+(define (eval-recv-buf-insert buf item)
+  (match buf
+    [(recv-buf* len val)
+     (if (>= (length val) len)
+         'recv-buf-full
+         (recv-buf* len (cons item val)))]))
+
+(define (eval-recv-buf->nat buf)
+  (define (bool-list->nat l pow)
+    (if (null? l)
+        0
+        (let ([e (expt 2 pow)]
+              [head (car l)]
+              [tail (cdr l)])
+          (if head
+              (+ e (bool-list->nat tail (+ 1 pow)))
+              (bool-list->nat tail (+ 1 pow))))))
+
+  (match buf
+    [(recv-buf* len val)
+     (if (not (= len (length val)))
+         'recv-buf-not-full
+         (bool-list->nat val 0))]))
 
 ;; Evaluate an expression. Takes an expression, context, and a state
 (define (evaluate-expr expression context state)
   (define (type-check? id val)
     (match (cxt-lookup id context)
       ['boolean (boolean? val)]
+      ['natural (natural? val)]
       ['channel (channel*? val)]
+      ['send-buf (send-buf*? val)]
+      ['recv-buf (recv-buf*? val)]
       [_ #f]))
 
-  (define (unary-op predicate next-func expr error-symbol)
+  (define (unary predicate next-func expr error-symbol)
     (let ([val (eval-helper expr)])
       (if (predicate val)
           (next-func val)
           error-symbol)))
-  
-  (define (binary-op predicate next-func expr-l expr-r error-symbol)
+
+  (define (symmetric predicate next-func expr-l expr-r error-symbol)
     (let ([val-l (eval-helper expr-l)]
           [val-r (eval-helper expr-r)])
       (if (and (predicate val-l)
@@ -53,22 +129,54 @@
           (next-func val-l val-r)
           error-symbol)))
 
+  (define (asymmetric pred-l pred-r next-func expr-l expr-r error-symbol)
+    (let ([val-l (eval-helper expr-l)]
+          [val-r (eval-helper expr-r)])
+      (if (and (pred-l val-l)
+               (pred-r val-r))
+          (next-func val-l val-r)
+          error-symbol)))
+
   (define (eval-helper expr)
     (match expr
-      [(message* e) (unary-op boolean? eval-message e 'message-type-err)]
-      [(value* c) (unary-op channel-full? channel*-value c 'value-type-err)]
-      [(not* e) (unary-op boolean? eval-not e 'not-type-err)]
-      [(and* l r) (binary-op boolean? eval-and l r 'and-type-err)]
-      [(or* l r) (binary-op boolean? eval-or l r 'or-type-err)]
-      [(eq?* l r) (binary-op boolean? eval-eq? l r 'eq-type-err)]
-      [(full?* c) (unary-op channel*? channel*-valid c 'full-type-err)]
+      ;; Boolean -> Boolean Expressions
+      [(not* e) (unary boolean? eval-not e 'not-type-err)]
+      [(and* l r) (symmetric boolean? eval-and l r 'and-type-err)]
+      [(or* l r) (symmetric boolean? eval-or l r 'or-type-err)]
+      [(eq* l r) (eval-helper (or* (and* l r) (and* (not* l) (not* r))))]
+      ;; Nat x Nat -> Nat Expressions
+      [(+* l r) (symmetric natural? eval-+ l r '+-type-err)]
+      ;; Nat x Nat -> Boolean Expressions
+      [(<* l r) (symmetric natural? eval-< l r '<-type-err)]
+      [(=* l r) (symmetric natural? eval-= l r '=-type-err)]
+      ;; Channel -> Boolean Expressions
+      [(full?* c) (unary channel*? channel*-valid c 'full-type-err)]
       [(empty?* c) (eval-helper (not* (full?* c)))]
+      ;; Channel Expressions
+      [(message* e) (unary boolean? eval-message e 'message-type-err)]
+      [(value* c) (unary channel-full? channel*-value c 'value-type-err)]
+      ;; Buffer -> Boolean Expressions
+      [(send-empty?* b) (unary send-buf*? eval-send-empty? b 'send-empty-type-err)]
+      [(recv-full?* b) (unary recv-buf*? eval-recv-full? b 'recv-full-type-err)]
+      ;; Buffer Expressions
+      [(nat->send-buf* s v) (symmetric natural? eval-nat->send-buf s v 'nat->send-buf-type-err)]
+      [(send-buf-head* b) (unary send-buf*? eval-send-buf-head b 'send-buf-head-type-err)]
+      [(send-buf-tail* b) (unary send-buf*? eval-send-buf-tail b 'send-buf-tail-type-err)]
+      [(empty-recv-buf* s) (unary natural? eval-empty-recv-buf s 'empty-recv-buf-type-err)]
+      [(recv-buf-insert* b v) (asymmetric recv-buf*?
+                                          boolean?
+                                          eval-recv-buf-insert b v
+                                          'recv-buf-insert-type-err)]
+      [(recv-buf->nat* b) (unary recv-buf*? eval-recv-buf->nat b 'recv-buf->nat-type-err)]
+      ;; Terminals
       [term
        (cond
          ;; 'empty reserved word
          [(eq? term 'empty) (channel* #f null)]
          ;; boolean literals
          [(boolean? term) term]
+         ;; natural numbers
+         [(natural? term) term]
          ;; all other symbols are variable references
          [(symbol? expr)
           (let ([val (state-get expr state)])
@@ -100,6 +208,10 @@
         [(and (eq? l-type 'boolean)
               (boolean? r-val))
          next-func]
+        ;; nat := nat
+        [(and (eq? l-type 'natural)
+              (natural? r-val))
+         next-func]
         ;; (undefined) channel := channel
         [(and (eq? l-type 'channel)
               (null? l-val)
@@ -117,8 +229,17 @@
               (channel*? r-val)
               (channel*-valid r-val))
          next-func]
+        ;; send-buf := send-buf
+        [(and (eq? l-type 'send-buf)
+              (send-buf*? r-val))
+         next-func]
+        ;; recv-buf := recv-buf
+        [(and (eq? l-type 'recv-buf)
+              (recv-buf*? r-val))
+         next-func]
+        ;; fall-through
         [else 'assignment-type-err])))
-  
+
   (define (filter-expressions exps-guards)
     (match exps-guards
       [(cons (cons exps guard) tail)
@@ -206,19 +327,63 @@
  (let* ([prog
          (unity*
           (declare* (list (cons 'reg 'boolean)
-                          (cons 'out 'channel)))
+                          (cons 'sbuf 'send-buf)
+                          (cons 'out 'channel)
+                          (cons 'rbuf 'recv-buf)
+                          (cons 'in 'channel)
+                          (cons 'counter 'natural)))
           (initially* (:=* (list 'reg
-                                 'out)
+                                 'sbuf
+                                 'out
+                                 'rbuf
+                                 'in
+                                 'counter)
                            (list #f
-                                 'empty)))
+                                 (nat->send-buf* 8 128)
+                                 'empty
+                                 (empty-recv-buf* 8)
+                                 (message* #t)
+                                 0)))
           (assign* (list (:=* (list 'reg
-                                    'out)
+                                    'sbuf
+                                    'out
+                                    'rbuf
+                                    'in
+                                    'counter)
                               (case* (list (cons (list (not* 'reg)
-                                                       (message* 'reg))
-                                                 (empty?* 'out))))))))]
+                                                       (send-buf-tail* 'sbuf)
+                                                       (message* (send-buf-head* 'sbuf))
+                                                       (recv-buf-insert* 'rbuf (value* 'in))
+                                                       'empty
+                                                       (+* 'counter 1))
+                                                 (and*
+                                                  (empty?* 'out)
+                                                  (and*
+                                                   (not* (send-empty?* 'sbuf))
+                                                   (and*
+                                                    (full?* 'in)
+                                                    (not* (recv-full?* 'rbuf))))))))))))]
         [env (interpret-declare prog)]
         [env2 (interpret-initially prog env)]
         [env3 (interpret-assign prog env2)])
    (and (environment*? env)
         (environment*? env2)
         (environment*? env3))))
+
+;; Assert that send-buf and recv-buf conversions work
+;; for all positive word sizes
+(define-symbolic W N integer?)
+(assert
+ (eq? (unsat)
+      (verify
+       #:assume (assert (and (< 0 W)
+                             (<= 0 N)
+                             (< N (expt 2 W))))
+       #:guarantee (assert
+                    (let* ([word-size W]
+                           [send-buffer (eval-nat->send-buf word-size N)]
+                           [send-word (send-buf*-val send-buffer)]
+                           [recv-word (reverse send-word)]
+                           [recv-buffer (recv-buf* word-size recv-word)]
+                           [recv-nat (eval-recv-buf->nat recv-buffer)])
+                      (eq? N recv-nat))))))
--- unity-synthesis/unity/syntax.rkt
+++ unity-synthesis/unity/syntax.rkt
@@ -2,6 +2,15 @@
 
 (require "../util.rkt")
 
+;; Types
+;; Boolean (true/false)
+;; Channel (full/empty containers for booleans)
+;; Nat (0, 1, 2...)
+;; Send-buf (N-ary lists of booleans in "big-endian")
+;; Recv-buf (N-ary lists of booleans in "little-endian")
+;;
+;; Terminals: #t, #f, 'empty, natural numbers
+
 ;; Channel types. Two fields:
 ;; 1) validity
 ;; 2) value
@@ -10,16 +19,18 @@
    value)
   #:transparent)
 
-;; Channel syntactic forms
-;; Construct a message Bool -> Channel
-(struct message*
-  (value)
+;; Send buffer type. Booleans in "big endian" order
+;; len is the original length
+(struct send-buf*
+  (len
+   val)
   #:transparent)
 
-;; Destruct a message Channel -> Bool
-;; Partial Function! Only defined for valid channels
-(struct value*
-  (channel)
+;; Receive buffer type. Booleans in "little endian" order
+;; len is the length when full
+(struct recv-buf*
+  (len
+   val)
   #:transparent)
 
 ;; Top-level syntax.
@@ -71,33 +82,113 @@
   #:transparent)
 
 ;; Expressions
-;; These terms combine into arbitrary boolean expressions.
-;; Terminals are: #t, #f, 'empty
-;; Variable reference
+
 ;; Negation (Bool -> Bool)
 (struct not* (exp) #:transparent)
+
 ;; Logical AND (Bool x Bool -> Bool)
 (struct and*
   (left
    right)
   #:transparent)
+
 ;; Logical OR (Bool x Bool -> Bool)
 (struct or*
   (left
    right)
   #:transparent)
+
 ;; Equality (Bool x Bool -> Bool)
-(struct eq?*
+(struct eq*
   (left
    right)
   #:transparent)
+
+;; Nat Addition
+(struct +*
+  (left
+   right)
+  #:transparent)
+
+;; Nat Less-Than
+(struct <*
+  (left
+   right))
+
+;; Nat Equality
+(struct =*
+  (left
+   right))
+
 ;; Channel not valid (Channel -> Bool)
 (struct empty?*
   (chan)
   #:transparent)
+
 ;; Channel valid (Channel -> Bool)
 (struct full?*
   (chan)
+  #:transparent)
+
+;; Send-buf* exhausted
+(struct send-empty?*
+  (buf)
+  #:transparent)
+
+;; Recv-buf* ready
+(struct recv-full?*
+  (buf)
+  #:transparent)
+
+;; Construct a message Bool -> Channel
+(struct message*
+  (value)
+  #:transparent)
+
+;; Destruct a message Channel -> Bool
+;; Partial Function! Only defined for valid channels
+(struct value*
+  (channel)
+  #:transparent)
+
+;; Construct a send buffer Nat x Nat -> Send-buf*
+(struct nat->send-buf*
+  (len
+   value)
+  #:transparent)
+
+;; The next (head) item in the send-buffer
+;; Partial function! Only defined for non-empty send-buf*
+;; Send-buf* -> Bool
+(struct send-buf-head*
+  (buf)
+  #:transparent)
+
+;; The remainder of the send-buffer
+;; Partial function! Only defined for non-empty send-buf*
+;; Send-buf* -> Send-buf*
+(struct send-buf-tail*
+  (buf)
+  #:transparent)
+
+;; Construct an empty recv buffer Nat -> Recv-buf*
+(struct empty-recv-buf*
+  (len)
+  #:transparent)
+
+;; Construct a new receive buffer with an item and an existing receive buffer
+;; Partial function! Only defined for non-full recv-buf*
+;; Recv-buf* x Bool -> Recv-buf*
+(struct recv-buf-insert*
+  (buf
+   item)
+  #:transparent)
+
+;; The natural number equivalent of a recv-buf*
+;; Partial function! Only defined for full recv-buf*
+;; Recv-buf* -> Nat
+(struct recv-buf->nat*
+  (buf)
   #:transparent)
 
 ;; Export the following from the module:
@@ -105,8 +196,14 @@
          channel*?
          channel*-valid
          channel*-value
-         message*
-         value*
+         send-buf*
+         send-buf*?
+         send-buf*-len
+         send-buf*-val
+         recv-buf*
+         recv-buf*?
+         recv-buf*-len
+         recv-buf*-val
          unity*
          declare*
          initially*
@@ -116,9 +213,23 @@
          not*
          and*
          or*
-         eq?*
+         eq*
+         +*
+         <*
+         =*
          empty?*
-         full?*)
+         full?*
+         send-empty?*
+         recv-full?*
+         message*
+         value*
+         nat->send-buf*
+         send-buf-head*
+         send-buf-tail*
+         empty-recv-buf*
+         recv-buf-insert*
+         recv-buf->nat*
+         )
 
 ;; Example syntax
 
