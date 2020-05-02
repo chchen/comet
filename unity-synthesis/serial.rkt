034fb725d1de605058952ebf4f9d2f814c11f51d
cchen <cchen@nougat.org>
2020-05-01 17:41:00 -0700

    Updates for serial synth suite

--- unity-synthesis/serial.rkt
+++ unity-synthesis/serial.rkt
@@ -3,7 +3,11 @@
 (require "unity/syntax.rkt"
          "arduino/synth.rkt"
          "arduino/channel.rkt"
-         "arduino/buffer.rkt")
+         "arduino/buffer.rkt"
+         "arduino/inversion.rkt"
+         "arduino/semantics.rkt"
+         "arduino/symbolic.rkt"
+         (prefix-in arduino: "arduino/environment.rkt"))
 
 (define channel-test
   (unity*
@@ -11,13 +15,14 @@
     (list (cons 'o 'send-channel)
           (cons 'i 'recv-channel)))
    (initially*
-    (:=* (list 'o)
-         (list 'empty)))
+    '())
    (assign*
-    (list (:=* (list 'o)
+    (list (:=* (list 'i 'o)
                (case*
-                (list (cons (list (message* #t))
-                            (empty?* 'o)))))))))
+                (list (cons (list 'empty
+                                  (message* (value* 'i)))
+                            (and* (full?* 'i)
+                                  (empty?* 'o))))))))))
 
 (define recv-buf-test
   (unity*
@@ -29,7 +34,11 @@
                'r)
          (list #f
                (empty-recv-buf* 8))))
-   '()))
+   (assign*
+    (list (:=* (list 'r)
+               (case*
+                (list (cons (recv-buf-put* 'r 'x)
+                            (not* (recv-buf-full?* 'r))))))))))
 
 (define send-buf-test
   (unity*
@@ -43,7 +52,9 @@
                (nat->send-buf* 8 1))))
    (assign*
     (list (:=* (list 'x)
-               (list (send-buf-get* 's)))))))
+               (case*
+                (list (cons (send-buf-get* 's)
+                            (not* (send-buf-empty?* 's))))))))))
 
 (define buf-test
   (unity*
@@ -59,6 +70,98 @@
                (empty-recv-buf* 8)
                (nat->send-buf* 8 42))))
    '()))
+
+(define type-test
+  (unity*
+   (declare*
+    (list (cons 'b 'boolean)
+          (cons 'n 'natural)
+          (cons 'r 'recv-buf)
+          (cons 's 'send-buf)
+          (cons 'i 'recv-channel)
+          (cons 'o 'send-channel)))
+   (initially*
+    (:=* (list 'b 'n 'r 's 'o)
+         (list #f
+               42
+               (empty-recv-buf* 8)
+               (nat->send-buf* 8 42)
+               'empty)))
+   (assign*
+    '())))
+
+;; (define (sym-count u)
+;;   (length (symbolics u)))
+
+;; (match type-test
+;;   [(unity*
+;;     (declare* unity-cxt)
+;;     initially
+;;     assign)
+;;    (let* ([mapping (unity-context->synth-map unity-cxt)]
+;;           [arduino-cxt (synth-map-arduino-context mapping)]
+;;           [state (symbolic-state arduino-cxt)]
+;;           [preds (append (channel-predicates type-test)
+;;                          (buffer-predicates type-test))])
+;;      (list
+;;       (cons 'expr
+;;             (map (lambda (d)
+;;                    (sym-count
+;;                     (exp?? d arduino-cxt '())))
+;;                  depths))
+;;       (cons 'expr-pred
+;;             (map (lambda (d)
+;;                    (sym-count
+;;                     (exp?? d arduino-cxt preds)))
+;;                  depths))
+;;       (cons 'eval-expr
+;;             (map (lambda (d)
+;;                    (sym-count
+;;                     (evaluate-expr (exp?? d arduino-cxt '())
+;;                                    arduino-cxt
+;;                                    state)))
+;;                  depths))
+;;       (cons 'eval-expr-pred
+;;             (map (lambda (d)
+;;                    (sym-count
+;;                     (evaluate-expr (exp?? d arduino-cxt preds)
+;;                                    arduino-cxt
+;;                                    state)))
+;;                  depths))))])
+
+;; (match type-test
+;;   [(unity*
+;;     (declare* unity-cxt)
+;;     initially
+;;     assign)
+;;    (let* ([mapping (unity-context->synth-map unity-cxt)]
+;;           [arduino-cxt (synth-map-arduino-context mapping)]
+;;           [preds (append (channel-predicates type-test)
+;;                          (buffer-predicates type-test))])
+;;      (map (lambda (e)
+;;             (map sym-count
+;;                  (map (lambda (d)
+;;                         (uncond-stmts?? d e arduino-cxt preds))
+;;                       '(1 2 3 4 5 6 7 8 9))))
+;;           '(0 1 2 3 4)))])
+
+;; (match type-test
+;;   [(unity*
+;;     (declare* unity-cxt)
+;;     initially
+;;     assign)
+;;    (let* ([mapping (unity-context->synth-map unity-cxt)]
+;;           [arduino-cxt (synth-map-arduino-context mapping)]
+;;           [preds (append (channel-predicates type-test)
+;;                          (buffer-predicates type-test))])
+;;      (map (lambda (e)
+;;             (map (lambda (s)
+;;                    (map sym-count
+;;                         (map (lambda (c)
+;;                                (cond-stmts?? c s e arduino-cxt preds))
+;;                              '(4))))
+;;                  '(4)))
+;;           '(4)))])
 
 (define sender
   (unity*
@@ -121,3 +224,6 @@
                                   (recv-buf->nat* 'buf))
                             (recv-buf-full?* 'buf)))))))))
 
+(define (snippets unity-prog)
+  (append (channel-predicates unity-prog)
+          (buffer-predicates unity-prog)))
