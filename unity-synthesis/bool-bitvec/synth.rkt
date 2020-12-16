6fb55745f8cd3012699aaff67e1cb2765f97a7cf
cchen <cchen@nougat.org>
2020-12-15 19:34:41 -0800

    Bool-bitvec synthesis finds traces correct for a concurrent model

--- unity-synthesis/bool-bitvec/synth.rkt
+++ unity-synthesis/bool-bitvec/synth.rkt
@@ -13,6 +13,11 @@
       (cons (car trace)
             (trim-trace (cdr trace) tail))))
 
+;; Synthesizes a bool-bitvec trace equal to the unity trace under mapping
+;;
+;; NOTE: This generates a trace that's correct for concurrent models.  For
+;; sequential correctness, you need to ensure that the synthesized trace
+;; satisfies the refinement simulation.
 (define (unity-trace->target-trace synth-map unity-guard unity-trace)
   (let* ([unity-ext-vars (synth-map-unity-external-vars synth-map)]
          [target-id-writable? (synth-map-target-id-writable? synth-map)]
@@ -23,24 +28,6 @@
          [unity-id->target-ids (synth-map-unity-id->target-ids synth-map)]
          [target-st (synth-map-target-state synth-map)]
          [unity-st (target-st->unity-st target-st)])
-
-    (define (monotonic-ordering unity-id trace-suffix)
-      (let* ([sketch-trace (begin
-                             (clear-asserts!)
-                             (trace-permutation?? trace-suffix))]
-             [trace-monotonic? (monotonic-ok? unity-id
-                                              target-st
-                                              (append sketch-trace target-st)
-                                              unity-st
-                                              unity-trace
-                                              target-st->unity-st)]
-             [model (synthesize
-                     #:forall target-st
-                     #:assume (assert unity-guard)
-                     #:guarantee (assert trace-monotonic?))])
-        (if (sat? model)
-            (evaluate sketch-trace model)
-            model)))
 
     (define (try-synth depth unity-id-val)
       (let* ([start-time (current-seconds)]
@@ -53,8 +40,8 @@
              [relevant-vals (append target-id-vals
                                     (relevant-values unity-val target-other-vals))]
              [sketch-trace (begin
-                          (clear-asserts!)
-                          (trace?? writable-ids relevant-vals depth target-st))]
+                             (clear-asserts!)
+                             (trace?? writable-ids relevant-vals depth target-st))]
              [mapped-val (unity-id->target-st->unity-val unity-id sketch-trace)]
              [model (synthesize
                      #:forall target-st
@@ -70,12 +57,8 @@
                            writable-ids)
                    (current-error-port))
           (if (sat? model)
-              (let* ([ext-var? (member unity-id unity-ext-vars)]
-                     [synthesized-trace (evaluate sketch-trace model)]
-                     [trace-suffix (trim-trace synthesized-trace target-st)])
-                (if ext-var?
-                    (monotonic-ordering unity-id trace-suffix)
-                    trace-suffix))
+              (let ([synthesized-trace (evaluate sketch-trace model)])
+                (trim-trace synthesized-trace target-st))
               (if (>= depth max-expression-depth)
                   model
                   (try-synth (add1 depth) unity-id-val))))))
